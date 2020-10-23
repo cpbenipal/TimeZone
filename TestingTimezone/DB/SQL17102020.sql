d3a55d160f1909e82d55d3cd550171e128d2ecd3
Beni Singh <cpbenipal@gmail.com>
2020-10-23 12:18:18 +0530

    final push

--- TestingTimezone/DB/SQL17102020.sql
+++ TestingTimezone/DB/SQL17102020.sql
@@ -1,6 +1,6 @@
 USE [DbTestTime]
 GO
-/****** Object:  Table [dbo].[tblScheduleTest]    Script Date: 16-10-2020 23:47:51 ******/
+/****** Object:  Table [dbo].[tblScheduleTest]    Script Date: 23-10-2020 12:14:11 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -18,7 +18,7 @@ CREATE TABLE [dbo].[tblScheduleTest](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[tblTopic]    Script Date: 16-10-2020 23:47:51 ******/
+/****** Object:  Table [dbo].[tblTopic]    Script Date: 23-10-2020 12:14:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -63,15 +63,16 @@ REFERENCES [dbo].[tblTopic] ([TopicId])
 GO
 ALTER TABLE [dbo].[tblScheduleTest] CHECK CONSTRAINT [FK_tblScheduleTest_tblTopic]
 GO
-/****** Object:  StoredProcedure [dbo].[spCreateTest]    Script Date: 16-10-2020 23:47:51 ******/
+/****** Object:  StoredProcedure [dbo].[spCreateTest]    Script Date: 23-10-2020 12:14:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 -- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
+-- Author:		Beni
+-- Create date: 16-10-2020
+-- Description:	Schedule a Test by Teacher
+-- Modifications:
 -- =============================================
 CREATE PROCEDURE [dbo].[spCreateTest] 
 @TopicId int, 
@@ -82,15 +83,16 @@ AS
 BEGIN 
 	BEGIN TRY 
 		BEGIN TRAN 
-		If not exists(select top 1 1 from tblScheduleTest where TopicId =@TopicId)
+		If not exists(select top 1 1 from tblScheduleTest 
+		    where TopicId=@TopicId And DATEDIFF(MINUTE,@StartDateTime,StartDateTime) = 0)
 		Begin
 				INSERT INTO tblScheduleTest
 				(TopicId, StartDateTime, Duration, TeacherTZ)
 				VALUES   (@TopicId, @StartDateTime, @Duration, @TeacherTZ)
-				SELECT 1
+				SELECT 1 
 		 End
 		 Else
-				SELECT 0
+				SELECT 0 
 		 COMMIT TRAN
 	END TRY  
 	BEGIN CATCH  
@@ -99,12 +101,12 @@ BEGIN
 		    PRINT 'ROLLBACK'
 			DECLARE  @ERRMSG NVARCHAR(1000) = ERROR_MESSAGE() + 
 			', Error on Line- ' + CONVERT(NVARCHAR, ERROR_LINE())  
-			SELECT @ERRMSG    			
+			SELECT @ERRMSG     			
 		RETURN  
 	END CATCH 
 END
 GO
-/****** Object:  StoredProcedure [dbo].[spGetAllTopics]    Script Date: 16-10-2020 23:47:51 ******/
+/****** Object:  StoredProcedure [dbo].[spGetAllTopics]    Script Date: 23-10-2020 12:14:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -128,7 +130,7 @@ BEGIN
 
 END
 GO
-/****** Object:  StoredProcedure [dbo].[spGetUpcomingTest]    Script Date: 16-10-2020 23:47:51 ******/
+/****** Object:  StoredProcedure [dbo].[spGetUpcomingTest]    Script Date: 23-10-2020 12:14:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -141,14 +143,14 @@ GO
 CREATE PROCEDURE [dbo].[spGetUpcomingTest] 
  	
 AS
-BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
+BEGIN 
+    
 	SET NOCOUNT ON;
 
-    -- Insert statements for procedure here
-	  
-	Select T.TopicId, T.Topic, S.StartDateTime, S.Duration from tblScheduleTest S
-	inner join tblTopic T on S.TopicId = T.TopicId order by S.StartDateTime desc 
-END
+	Select T.TopicId, T.Topic, S.StartDateTime, S.Duration,
+	DATEADD(mi, Convert(int,LTRIM(DATEDIFF(MINUTE, 0, S.Duration ))),S.StartDateTime) as EndDateTime 
+	From tblScheduleTest S inner join tblTopic T on S.TopicId = T.TopicId
+	Where DATEDIFF(MINUTE,GETUTCDATE(), DATEADD(mi, Convert(int,LTRIM(DATEDIFF(MINUTE, 0, S.Duration ))),S.StartDateTime)) >= 0 
+
+END 
 GO
